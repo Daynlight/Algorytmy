fac06daa88f56e4cb232b520249c43a46f4291b9
🐢D A Y N L I G H T🐢 <danielstodulski000@gmail.com>
2024-11-08 12:30:08 +0000

    Multiple same flags only --

--- Algorytmy/Main.cpp
+++ Algorytmy/Main.cpp
@@ -4,15 +4,19 @@
 
 int main(int args, const char *argv[]) {
 #ifdef NDEBUG
-	std::unordered_map<std::string, std::vector<int>> argv_map; // [key] {bool, val1, val2 ...}
+	std::vector<std::pair<std::string, std::vector<int>>> argv_map; // {[key] {val1, ...}, [key2] {val1, ...}}
+	
 	std::chrono::nanoseconds time;
-
 	std::string key = "";
+	int index = -1;
+
 	for(int i = 1; i < args; i++){
 		if(argv[i][0] == '-'){
 			if(argv[i][1] == '-'){
+				index++;
 				key = argv[i];
-				argv_map[key].emplace_back(1);
+				argv_map.emplace_back(std::pair<std::string, std::vector<int>>());
+				argv_map[index].first = key;
 			}
 		}
 		else{
@@ -20,75 +24,77 @@ int main(int args, const char *argv[]) {
 			strVal << argv[i];
 			int intVal;
 			strVal >> intVal;
-			argv_map[key].emplace_back(intVal);
+			argv_map[index].second.emplace_back(intVal);
 		}
 	}
 	
-	if (argv_map.find("--help") != argv_map.end()) {
-		printf("\n###################################################################\n");
-		printf("############################ Algorytmy ############################\n");
-		printf("###################################################################\n\n");
-		printf("--clock - Check clock accuracy\n");
-		printf("--nwd [a] [b] [recursive] - Print NWD value\n");
-		printf("--pow [a] [n] [recursive] - Print POW value\n");
-		printf("--silnia [a] [recursive] - Print Silnia value\n");
-		printf("--fibonachi [n] [recursive] - Print Fibonachi n element\n");
-		printf("--suma [elementy] [min] [max] - Show tests for sum algorytms\n");
-		printf("--sort [elementy] [min] [max] - Show tests for sort algorytms\n");
-		return 0;
-	}
-	if (argv_map.find("--clock") != argv_map.end()) 
-		checkClock();
-	if (argv_map.find("--nwd") != argv_map.end()){
-		int a = argv_map["--nwd"][1];
-		int b = argv_map["--nwd"][2];
-		if(!argv_map["--nwd"][3]){
-			 time = Time([&a, &b]{ printf("nwd(%d, %d) = %d | ", a, b, nwdNormal(a, b)); });
-			 printTime(time);
+	for(int i = 0; i < argv_map.size(); i++){
+		if (argv_map[i].first == "--help") {
+			printf("\n###################################################################\n");
+			printf("############################ Algorytmy ############################\n");
+			printf("###################################################################\n\n");
+			printf("--clock - Check clock accuracy\n");
+			printf("--nwd [a] [b] [recursive] - Print NWD value\n");
+			printf("--pow [a] [n] [recursive] - Print POW value\n");
+			printf("--silnia [a] [recursive] - Print Silnia value\n");
+			printf("--fibonachi [n] [recursive] - Print Fibonachi n element\n");
+			printf("--suma [elementy] [min] [max] - Show tests for sum algorytms\n");
+			printf("--sort [elementy] [min] [max] - Show tests for sort algorytms\n");
+			return 0;
 		}
-		else{
-				time = Time([&a, &b]{ printf("nwd(%d, %d) = %d | ", a, b, nwdRecursive(a, b)); }); 
+		if (argv_map[i].first == "--clock") 
+			checkClock();
+		if (argv_map[i].first == "--nwd"){
+			int a = argv_map[i].second[0];
+			int b = argv_map[i].second[1];
+			if(!argv_map[i].second[2]){
+				time = Time([&a, &b]{ printf("nwd(%d, %d) = %d | ", a, b, nwdNormal(a, b)); });
 				printTime(time);
+			}
+			else{
+					time = Time([&a, &b]{ printf("nwd(%d, %d) = %d | ", a, b, nwdRecursive(a, b)); }); 
+					printTime(time);
+			}
 		}
-	}
-	if (argv_map.find("--pow") != argv_map.end()){
-		int a = argv_map["--pow"][1];
-		int n = argv_map["--pow"][2];
-		if(!argv_map["--pow"][3]){
-			time = Time([&a, &n]{ printf("%d^%d = %d | ", a, n, powNormal(a, n)); });
+		if (argv_map[i].first == "--pow"){
+			int a = argv_map[i].second[0];
+			int n = argv_map[i].second[1];
+			if(!argv_map[i].second[2]){
+				time = Time([&a, &n]{ printf("%d^%d = %d | ", a, n, powNormal(a, n)); });
+				printTime(time);
+			}
+			else{
+				time = Time([&a, &n]{ printf("%d^%d = %d | ", a, n, powRecursive(a, n)); }); 
+				printTime(time);
+			}
+		}
+		if (argv_map[i].first == "--silnia"){
+			int a = argv_map[i].second[0];
+			if(!argv_map[i].second[1]){
+				time = Time([&a]{ printf("%d! = %d | ", a, silniaNormal(a)); });
+				printTime(time);
+			}
+			else{
+				time = Time([&a]{ printf("%d! = %d | ", a, silniaRecursive(a)); }); 
+				printTime(time);
+			}
+		}
+		if (argv_map[i].first == "--fibonachi"){
+			int n = argv_map[i].second[0];
+			if(!argv_map[i].second[1]){
+			time = Time([&n]{ printf("fibonachi(%d) = %d | ", n, fibonachiNormal(n)); });
 			printTime(time);
-		}
-		else{
-			time = Time([&a, &n]{ printf("%d^%d = %d | ", a, n, powRecursive(a, n)); }); 
+			}
+			else{
+			time = Time([&n]{ printf("fibonachi(%d) = %d | ", n, fibonachiRecursive(n)); }); 
 			printTime(time);
+			}
 		}
+		if (argv_map[i].first == "--suma") 
+			sumaTest(argv_map[i].second[0], argv_map[i].second[1], argv_map[i].second[2]);
+		if (argv_map[i].first == "--sort")
+			sortTest(argv_map[i].second[0], argv_map[i].second[1], argv_map[i].second[2]);
 	}
-	if (argv_map.find("--silnia") != argv_map.end()){
-		int a = argv_map["--silnia"][1];
-		if(!argv_map["--silnia"][2]){
-			time = Time([&a]{ printf("%d! = %d | ", a, silniaNormal(a)); });
-			printTime(time);
-		}
-		else{
-			time = Time([&a]{ printf("%d! = %d | ", a, silniaRecursive(a)); }); 
-			printTime(time);
-		}
-	}
-	if (argv_map.find("--fibonachi") != argv_map.end()){
-		int n = argv_map["--fibonachi"][1];
-		if(!argv_map["--fibonachi"][2]){
-		time = Time([&n]{ printf("fibonachi(%d) = %d | ", n, fibonachiNormal(n)); });
-		printTime(time);
-		}
-		else{
-		time = Time([&n]{ printf("fibonachi(%d) = %d | ", n, fibonachiRecursive(n)); }); 
-		printTime(time);
-		}
-	}
-	if (argv_map.find("--suma") != argv_map.end()) 
-		sumaTest(argv_map["--suma"][1], argv_map["--suma"][2], argv_map["--suma"][3]);
-	if (argv_map.find("--sort") != argv_map.end()) 
-		sortTest(argv_map["--sort"][1], argv_map["--sort"][2], argv_map["--sort"][3]);
 #else
 	checkClock();
 	
